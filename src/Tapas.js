ab913ef6f16ada40e3f5abae1058de74db989139
Rodrigo Peinado <rodrigoap@arnet.com.ar>
2018-06-23 13:00:56 -0300

    added histogram #5

--- src/Tapas.js
+++ src/Tapas.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Badge } from 'react-bootstrap';
 import Tapa from './Tapa';
+import Histogram from './Histogram';
 
 /**
  * Tapas
@@ -13,9 +14,28 @@ class Tapas extends Component {
   render() {
     const { from } = this.props;
     const { to } = this.props;
-    console.log(`${from} -> ${to}`);
+    console.log(`${from} -> ${to} of ${this.props.tapas.length}`);
+
+    const labels = [];
+    const data = [];
+    let currentYear = 0;
+    let currentIndex = -1;
+    for (let i = 0; i < this.props.tapas.length; i++) {
+      const tapa = this.props.tapas[i];
+      if (tapa.year === currentYear) {
+        data[currentIndex] = data[currentIndex] + 1;
+      } else {
+        currentIndex++;
+        currentYear = tapa.year;
+        labels.push(`${currentYear}`);
+        data.push(1);
+      }
+    }
     return (
       <div>
+        {this.props.tapas.length > 0 && (
+          <Histogram xLabels={labels} yValues={data} />
+        )}
         <div className="page-nav">
           <span className="button-nav-prev">
             <Button bsStyle="primary" onClick={this.props.previousPage}>
